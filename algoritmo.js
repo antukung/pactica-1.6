ae2f266e46bf26bb91d04b574ec6af62ebc79307
Antu <122485403+antukung@users.noreply.github.com>
2023-03-31 11:27:16 -0300

    nuevo comit con toda la practica
    aca esta el commit con todo

--- algoritmo.js
+++ algoritmo.js
@@ -1,8 +1,25 @@
-let nombre = prompt ("cual es tu nombre?");
+let entero = prompt("ingresar un numero entero");
+let numerodecimal = prompt("ingresa un numero decimal");
+let boleano = prompt ("ingresa un dato boleano");
+let caracter = prompt ("ingresa un carater");
+let palabra = prompt ("ingresa una palabra");
+
+document.write("<h1> Entero:" + entero + "</h1>");
+document.write("<h2>Numero decimal:" + numerodecimal + "</h2>");
+document.write("<h3>Boleano:" + boleano + "</h3>");
+document.write("<h4>Caracter:" + caracter + "</h4>");
+document.write("<h5>Palabra:" +palabra + "</h5>");
+
+
+
+
+
+
+/* let nombre = prompt ("cual es tu nombre?");
 
 console.log(nombre);
 
 document.querySelector("body").innerHTML= "<h1>"+ nombre + "</h1>";
 
-document.write("<h2> Mi nombre es " + nombre + "!</h2>");
+document.write("<h2> Mi nombre es " + nombre + "!</h2>"); */
 
